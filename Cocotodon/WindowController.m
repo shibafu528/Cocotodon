40601779ce1d1c70ae8c91bf6c6ee3d720a792a8
shibafu <shibafu528@gmail.com>
2021-06-26 13:03:29 +0900

    お知らせボタンの有効化制御

--- Cocotodon/WindowController.m
+++ Cocotodon/WindowController.m
@@ -6,7 +6,8 @@
 
 @interface WindowController ()
 
-@property (nonatomic, weak) IBOutlet NSSegmentedControl *toolbarAnnouncementItem;
+@property (nonatomic, weak) IBOutlet NSToolbarItem *toolbarAnnouncementItem;
+@property (nonatomic, weak) IBOutlet NSSegmentedCell *toolbarAnnouncementCell;
 
 @property (nonatomic) NSArray<DONMastodonAnnouncement*> *announcements;
 
@@ -22,8 +23,22 @@
         self.window.toolbarStyle = NSWindowToolbarStyleExpanded;
     }
     
+    self.toolbarAnnouncementItem.enabled = NO;
     [App.client announcementsWithSuccess:^(NSURLSessionDataTask * _Nonnull task, NSArray<DONMastodonAnnouncement *> * _Nonnull results) {
         self.announcements = results;
+        if (results.count) {
+            self.toolbarAnnouncementItem.enabled = YES;
+            if (@available(macOS 11.0, *)) {
+                [self.toolbarAnnouncementCell setImage:[NSImage imageWithSystemSymbolName:@"megaphone.fill" accessibilityDescription:nil]
+                                            forSegment:0];
+            }
+        } else {
+            self.toolbarAnnouncementItem.enabled = NO;
+            if (@available(macOS 11.0, *)) {
+                [self.toolbarAnnouncementCell setImage:[NSImage imageWithSystemSymbolName:@"megaphone" accessibilityDescription:nil]
+                                              forSegment:0];
+            }
+        }
     }
                                  failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nullable error) {
         WriteAFNetworkingErrorToLog(error);
